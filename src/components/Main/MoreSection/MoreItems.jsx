633c55aff031d03a572755ad1a07ee1795f9f974
Vitalii <vitaliy.kalmykov.work@gmail.com>
2025-01-14 14:36:52 +0200

    add expand functionality to more section

--- src/components/Main/MoreSection/MoreItems.jsx
+++ src/components/Main/MoreSection/MoreItems.jsx
@@ -6,31 +6,59 @@ const MoreItems = ({item, onExpand, expandedId}) => {
         <div
             className={`group
             border-2
-            flex items-center justify-center flex-col
+            flex items-center flex-col
             font-bold text-2xl
             shadow-xl rounded-2xl
             transition-all duration-300 ease-in
-            hover:scale-110 
-                       ${expandedId === item.id ? 'absolute inset-0 z-50 bg-white' : 'h-full'}`} // Використовуємо h-auto для розширення
+                       ${expandedId === item.id ? 'absolute inset-0 z-50 bg-white justify-start' : 'hover:scale-110 h-full justify-center'}`}
         >
                     <span
-                        className="relative mt-8
+                        className={`relative mt-8
                         border-transparent
                         transition-all duration-300 ease-in
-                        group-hover:border group-hover:border-blue-500 group-hover:px-2"
+                        ${expandedId !== item.id ? 'group-hover:border group-hover:border-blue-500 group-hover:px-2' 
+                            : 
+                            'border p-2 border-blue-500'
+                        }
+                        `}
                     >
                         {item.text}
                     </span>
+
             <Button type="button"
-                    className="
+                    className={`
                     border-2
                     mt-2 p-2
                     rounded-2xl
                     opacity-0 group-hover:opacity-100
-                    transition-opacity duration-300"
+                    transition-opacity duration-300
+                    ${expandedId === item.id ? 'hidden' : ''}
+                    `}
                     onClick={() => onExpand(item.id)}
             >
                 look more
+            </Button>
+
+            <Button type="button"
+                    className={`
+                     border-2
+                     w-8 h-8
+                     flex items-center justify-center
+                     rounded-full
+                    ${expandedId === item.id ? 'absolute top-2 right-2' : 'hidden'}
+                    `}
+                    onClick={() => onExpand(item.id)}
+            >
+
+                <div className="relative w-full">
+                    <div
+                        className="absolute inset-0 w-full h-[2px] bg-black rotate-45 origin-center"
+                    ></div>
+                    <div
+                        className="absolute inset-0 w-full h-[2px]  bg-black -rotate-45 origin-center"
+                    ></div>
+                </div>
+
             </Button>
         </div>
     );
